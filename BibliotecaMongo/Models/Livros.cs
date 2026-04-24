3d7d2925376bbaafbcbd0779683fd29fd1559911
guilherme0903 <guilhermegui0@hotmail.com>
2026-04-23 22:41:26 -0300

    add crud inicial

--- BibliotecaMongo/Models/Livros.cs
+++ BibliotecaMongo/Models/Livros.cs
@@ -1,7 +1,11 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 namespace BibliotecaMongo.Models;
 
 public class Livro
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
     public string Titulo { get; set; }
     public string Autor { get; set; }
--- BibliotecaMongo/obj/Debug/net10.0/BibliotecaMongo.AssemblyInfo.cs
+++ BibliotecaMongo/obj/Debug/net10.0/BibliotecaMongo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaMongo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca370712684343a9d430dc6de8900b645e4b1da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c321199f0ed5d908b974101504692768dfe600f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaMongo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaMongo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
